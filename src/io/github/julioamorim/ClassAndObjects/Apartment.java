cf7faa9d0182fcf8ba64d92c08ea58140fa15ed0
Júlio Amorim <julio.l.amorim00@gmail.com>
2020-06-22 14:37:59 -0300

    Class and objects example and simple manipulation with superfitial definition

--- src/io/github/julioamorim/ClassAndObjects/Apartment.java
+++ src/io/github/julioamorim/ClassAndObjects/Apartment.java
@@ -3,10 +3,18 @@ package io.github.julioamorim.ClassAndObjects;
 //class definition
 //a class can represent / abstract any aspect of the real world
 public class Apartment {
+
     public int sizeM2Apartment;
     public int floorsApartment;
     public double valuationApartment;
     public int vacanciesNumberApartment;
+
+    public Apartment(int sizeM2Apartment, int floorsApartment, double valuationApartment, int vacanciesNumberApartment) {
+        this.sizeM2Apartment = sizeM2Apartment;
+        this.floorsApartment = floorsApartment;
+        this.valuationApartment = valuationApartment;
+        this.vacanciesNumberApartment = vacanciesNumberApartment;
+    }
 
     public int getSizeM2Apartment() {
         return sizeM2Apartment;
@@ -38,5 +46,15 @@ public class Apartment {
 
     public void setVacanciesNumberApartment(int vacanciesNumberApartment) {
         this.vacanciesNumberApartment = vacanciesNumberApartment;
+    }
+
+    @Override
+    public String toString() {
+        return "Apartment{" +
+                "sizeM2Apartment=" + sizeM2Apartment +
+                ", floorsApartment=" + floorsApartment +
+                ", valuationApartment=" + valuationApartment +
+                ", vacanciesNumberApartment=" + vacanciesNumberApartment +
+                '}';
     }
 }
---
+++ src/io/github/julioamorim/ClassAndObjects/Main.java
@@ -0,0 +1,13 @@
+package io.github.julioamorim.ClassAndObjects;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //object definition
+
+        //the apartment is now represented in memory as an object. We have the representation in memory that there is an object type "x", with its respective values.
+        Apartment julioApartment = new Apartment(400, 19, 1.000000, 2);
+        System.out.println(julioApartment);
+
+    }
+}
